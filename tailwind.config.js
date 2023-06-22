8cb7511b0507d8d1a79f542808afa649e61f3cd8
Nathan Lefetey <lefetey.nathan@gmail.com>
2023-06-22 18:10:07 +0200

    :lipstick: Add style.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,css,js'],
+  content: ['./*.{html,css,js}'],
   theme: {
     extend: {
       fontFamily: {
@@ -39,4 +39,3 @@ module.exports = {
   },
   plugins: []
 }
-
