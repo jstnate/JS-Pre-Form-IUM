95247ae8b496f5292be8de7cf04be6d0b9f796ad
Nathan Lefetey <lefetey.nathan@gmail.com>
2023-06-23 13:19:08 +0200

    :art: Improve folder structure

--- pre-form-IUM/tailwind.config.js
+++ pre-form-IUM/tailwind.config.js
@@ -26,14 +26,19 @@ module.exports = {
       },
       height: {
         'responsive': '70vh',
-        'preform-fieldset-h': '400px'
+        'preform-fieldset-h': '400px',
+        'calc-fieldset-h': 'calc(400px - 64px)'
       },
       width: {
         'preform-fieldset-w': '800px',
+        'calc-fieldset-w': 'calc(800px - 96px)',
         'auto': 'auto'
       },
       boxShadow: {
         'fieldset-shadow': '0 8px 16px -5px rgba(0,0,0,0.15)'
+      },
+      translate: {
+        'slide-1': 'calc(800px + 96px)'
       }
     }
   },
